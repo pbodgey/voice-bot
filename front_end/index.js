6701b38c566474d35028ade2daae12207dffaec1
AaronTheNerd <bargeaaron@gmail.com>
2022-04-25 20:58:45 -0400

    Created application entry-point

---
+++ front_end/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+//const webhook = require('./webhook.js');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (request, response) {
+    response.sendFile(path.join(__dirname, 'public/html/index.html'));
+});
+
+var server = app.listen(5000, function() {
+    console.log('Node server is running...');
+});

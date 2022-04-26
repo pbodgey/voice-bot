570de67b277288e940fdd6db1e32cd470cb60a6a
AaronTheNerd <bargeaaron@gmail.com>
2022-04-25 22:07:28 -0400

    Removed unnecessary config import

--- front_end/index.js
+++ front_end/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 
-//const webhook = require('./webhook.js');
-
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
